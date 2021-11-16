041b3c8fa809b002429cad98c25c26c6a10adde3
IlayGor <ilaygor97@gmail.com>
2021-11-16 14:50:08 +0300

    Отображение бд пользователей

--- venv/irsDB/main.py
+++ venv/irsDB/main.py
@@ -4,6 +4,7 @@ from models import *
 from GUI import *
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QTableWidgetItem as twi
 #pyuic5 -x base.ui -o gui.py
 """
 with db:
@@ -22,6 +23,9 @@ with db:
 
 
 class UImodif(Ui_MainWindow):
+
+    otype = "user"
+
     def btnFunction(self):
         self.loginBtn.clicked.connect(self.login)
         self.addBtn.clicked.connect(lambda: self.redirect(2))
@@ -31,6 +35,7 @@ class UImodif(Ui_MainWindow):
 
     def login(self):
         self.stackedWidget.setCurrentIndex(4)
+        self.usrTable()
         """
         print(self.loginFld.text(), self.passFld.text())
         try:
@@ -45,7 +50,11 @@ class UImodif(Ui_MainWindow):
 
 
     def redirect(self, n):
-        self.stackedWidget.setCurrentIndex(n)
+        if self.otype == "user":
+            print("user")
+
+        else:
+            self.stackedWidget.setCurrentIndex(n)
 
 
     def newImg(self):
@@ -66,6 +75,50 @@ class UImodif(Ui_MainWindow):
                weldingProgram = weldingProgram, processingTime = processingTime, img = 1).save()
         except:
             print("не создано")
+
+    def table(self):
+        self.tableWidget.setColumnCount(5)
+        self.tableWidget.setHorizontalHeaderLabels(["Номер чертежа", "Наименование", "Марка материала","Программа сварки","Время обработки"])
+        details = Detail.select()
+        self.tableWidget.setRowCount(len(details))
+        for detail in details:
+            print(detail.detailName)
+        self.tableWidget.resizeColumnsToContents()
+
+    def usrTable(self):
+        self.tableWidget.setColumnCount(10)
+        self.tableWidget.setHorizontalHeaderLabels(
+            ["Логин", "Имя", "Пароль", "Пользователи", "Детали", "Соединения", "Протоколы", "Архивы", "Добавление", "Удаление"])
+        users = User.select()
+        self.tableWidget.setRowCount(len(users))
+        for i in range(len(users)):
+            print(users[i].name)
+            self.tableWidget.setItem(i, 0, twi(users[i].login))
+            self.tableWidget.setItem(i, 1, twi(users[i].name))
+            self.tableWidget.setItem(i, 2, twi(users[i].passWord))
+            self.tableWidget.setCellWidget(i, 3, QtWidgets.QCheckBox())
+            self.tableWidget.setCellWidget(i, 4, QtWidgets.QCheckBox())
+            self.tableWidget.setCellWidget(i, 5, QtWidgets.QCheckBox())
+            self.tableWidget.setCellWidget(i, 6, QtWidgets.QCheckBox())
+            self.tableWidget.setCellWidget(i, 7, QtWidgets.QCheckBox())
+            self.tableWidget.setCellWidget(i, 8, QtWidgets.QCheckBox())
+            self.tableWidget.setCellWidget(i, 9, QtWidgets.QCheckBox())
+            self.tableWidget.cellWidget(i, 3).setCheckState(
+                QtCore.Qt.Checked if users[i].accessUser else QtCore.Qt.Unchecked)
+            self.tableWidget.cellWidget(i, 4).setCheckState(
+                QtCore.Qt.Checked if users[i].accessDetail else QtCore.Qt.Unchecked)
+            self.tableWidget.cellWidget(i, 5).setCheckState(
+                QtCore.Qt.Checked if users[i].accessConn else QtCore.Qt.Unchecked)
+            self.tableWidget.cellWidget(i, 6).setCheckState(
+                QtCore.Qt.Checked if users[i].accessProt else QtCore.Qt.Unchecked)
+            self.tableWidget.cellWidget(i, 7).setCheckState(
+                QtCore.Qt.Checked if users[i].accessArch else QtCore.Qt.Unchecked)
+            self.tableWidget.cellWidget(i, 8).setCheckState(
+                QtCore.Qt.Checked if users[i].accessAdd else QtCore.Qt.Unchecked)
+            self.tableWidget.cellWidget(i, 9).setCheckState(
+                QtCore.Qt.Checked if users[i].accessRemove else QtCore.Qt.Unchecked)
+
+        self.tableWidget.resizeColumnsToContents()
 
 
 
